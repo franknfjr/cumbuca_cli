04ff6279f732f0c3ff246a1b60e2e82473e69274
franknfjr <frankdonascimentojr@gmail.com>
2024-07-17 11:53:27 -0300

    feat: add escript configuration for CLI execution

--- mix.exs
+++ mix.exs
@@ -1,28 +1,21 @@
 defmodule CumbucaCli.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :cumbuca_cli,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      escript: [main_module: CumbucaCLI]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
